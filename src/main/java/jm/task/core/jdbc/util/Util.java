1a9405e327c56814477ba06d07460f0c7da83422
aleksey-komarov <zloy.komarik13@gmail.com>
2024-11-01 14:25:17 +0600

    2

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,6 +1,7 @@
 package jm.task.core.jdbc.util;
 
 import jm.task.core.jdbc.model.User;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.cfg.Environment;
@@ -16,6 +17,7 @@ public class Util {
     private static final String USERNAME = "root";
     private static final String PASSWORD = "root";
     private static final String DIALECT = "org.hibernate.dialect.MySQL8Dialect";
+    private static SessionFactory sessionFactory;
 
     public static Connection getConnection() throws ClassNotFoundException, SQLException {
         Connection connection = null;
@@ -24,15 +26,19 @@ public class Util {
     }
 
     public static SessionFactory getSessionFactory() throws Exception {
-        Properties properties = new Properties();
-        properties.put(Environment.DRIVER, DRIVER);
-        properties.put(Environment.URL, URL);
-        properties.put(Environment.USER, USERNAME);
-        properties.put(Environment.PASS, PASSWORD);
-        properties.put(Environment.DIALECT, DIALECT);
-        properties.put(Environment.SHOW_SQL, "true");
-        properties.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
-        properties.put(Environment.HBM2DDL_AUTO, "");
-        return new Configuration().addProperties(properties).addAnnotatedClass(User.class).buildSessionFactory();
+        if (sessionFactory == null) {
+            System.out.println("Creating session factory");
+            Properties properties = new Properties();
+            properties.put(Environment.DRIVER, DRIVER);
+            properties.put(Environment.URL, URL);
+            properties.put(Environment.USER, USERNAME);
+            properties.put(Environment.PASS, PASSWORD);
+            properties.put(Environment.DIALECT, DIALECT);
+            properties.put(Environment.SHOW_SQL, "true");
+            properties.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+            properties.put(Environment.HBM2DDL_AUTO, "");
+            sessionFactory = new Configuration().addProperties(properties).addAnnotatedClass(User.class).buildSessionFactory();
+        }
+        return sessionFactory;
     }
 }
